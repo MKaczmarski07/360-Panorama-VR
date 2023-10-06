907e51ad41a7b9c74a507a8a8cb07656ff336047
Mikołaj Kaczmarski <mikolajkaczmarski07@onet.pl>
2023-10-06 19:13:06 +0200

    Added mode changing info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,7 @@ const app = initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, VrComponent, LoaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/vr/vr.component.ts
+++ src/app/home/vr/vr.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { VrThreeService } from 'src/app/vr-three.service';
+import { FullscreenService } from 'src/app/fullscreen.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-vr',
   templateUrl: './vr.component.html',
   styleUrls: ['./vr.component.scss'],
+  animations: [
+    trigger('toggleVisibility', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translate(50%, 100%)',
+        }),
+        animate(
+          '100ms ease-out',
+          style({ opacity: 1, transform: 'translate(50%, 0)' })
+        ),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('100ms ease-out', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class VrComponent implements OnInit {
-  constructor(public vrThreeService: VrThreeService) {}
+  controlsInfo = '';
+
+  constructor(
+    public vrThreeService: VrThreeService,
+    public fullscreenService: FullscreenService
+  ) {}
 
   ngOnInit(): void {
     this.vrThreeService.createScene();
+  }
+
+  showControlInfo() {
+    if (this.vrThreeService.orbitControlMode) {
+      this.controlsInfo = 'Orientation';
+      setTimeout(() => {
+        this.controlsInfo = '';
+      }, 4000);
+    } else {
+      this.controlsInfo = 'Orbit';
+      setTimeout(() => {
+        this.controlsInfo = '';
+      }, 4000);
+    }
   }
 }
